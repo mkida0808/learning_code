052a8ecf3884301fab5ba6f54abc7a910970fa51
mkida0808 <mkida0808@yahoo.co.jp>
2021-06-20 01:47:52 +0900

    add fetch_mode and emulate_prepares

--- todo_app_php_v4-main/work/app/exception.php
+++ todo_app_php_v4-main/work/app/exception.php
@@ -7,6 +7,8 @@ try {
     DB_PASS,
     [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+      PDO::ATTR_EMULATE_PREPARES => false,
     ]
   );
 } catch (PDOException $e) {
